a5b50b422850e938a3f0908da81cbdf5ce4d24c3
Adeeba <124208667+AdeebaSaeed@users.noreply.github.com>
2023-05-15 21:10:55 +0200

    Update server.js

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,7 +10,31 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    if (req.method === 'GET' && req.url === '/state') {
+      res.writeHead(200, { 'Content-Type' : 'application/json'});
+      res.end(JSON.stringify({state: state}));
+    }
+    else if (req.method === 'GET' && req.url === '/reset') {
+      state = 10;
+      res.writeHead(200, { 'Content-Type' : 'application/json'});
+      res.end(JSON.stringify({ state:state}));
+    }
+    else if (req.method === 'GET' && req.url === '/add') {
+      state++;
+      res.writeHead(200, { 'Content-Type': 'application/json'});
+      res.end(JSON.stringify({state: state}));
+    }
+    else if (req.method === 'GET' && req.url === '/subtract') {
+      state--;
+      res.writeHead(200, { 'Content-Type' : 'application/json'});
+      res.end(JSON.stringify({state: state}));
+    }
+    else {
+      res.writeHead(404, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify({error: 'Not found'}));
+    }
   });
+  
 
   return server;
 }
